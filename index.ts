3f1d1e0835bc53d87a01db7c4a179834f5fec4de
andreyqpi <andreytome2@gmail.com>
2025-07-03 20:46:29 -0300

    avançando no backend

--- index.ts
+++ index.ts
@@ -1,17 +1,37 @@
-import express from "express";
+import express, { json } from "express";
 import cors from "cors"
+import { PrismaClient } from './generated/prisma'
+import argon2 from 'argon2'
+
+const prisma = new PrismaClient()
 
 const app = express();
 
 app.use(cors())
+app.use(json())
 
 const port = 3000;
 
 app.get("/", (req, res) => {
-    res.send("Hello Word")
+res.send("hello world")
 });
 
-app.listen((port) =>{
-    console.log(error);
-    
+app.post("/auth/user", async(req,res) => {
+const {body} =req
+if (!body.email ||  !body.password) {
+    res.status(400).send({
+        message: "resquisição invalida"
+    })
+    return;
+}
+
+body.password = await argon2.hash(body.password)
+
+const createdUser = await prisma.user.create (body);
+res.send(createdUser)
+})
+
+app.listen(port, () => {
+    console.log("app iniciado na porta:" + port);
+
 })
