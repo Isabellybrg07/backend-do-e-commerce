12a1ce19f498cdab71369cfe473e563b98138d33
andreyqpi <andreytome2@gmail.com>
2025-07-03 20:24:48 -0300

    first commit

---
+++ index.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors"
+
+const app = express();
+
+app.use(cors())
+
+const port = 3000;
+
+app.get("/", (req, res) => {
+    res.send("Hello Word")
+});
+
+app.listen((port) =>{
+    console.log(error);
+    
+})
